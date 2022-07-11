25cde3125eb34d8af083b2d119487c3df04b1329
PurplProto <pairedprototype@hotmail.com>
2022-07-11 01:45:57 +0100

    Parse a list of mods and server mods

--- launcher-app/src/pre_launch.rs
+++ launcher-app/src/pre_launch.rs
@@ -1,10 +1,31 @@
 use crate::app_config::PreLaunchSettings;
-use std::process::Command;
+use std::{fs, path::PathBuf, process::Command};
 
-pub(crate) fn handle_env_setup(config: PreLaunchSettings) {
+#[derive(Debug)]
+pub struct PreLaunchMeta {
+    pub has_mods: bool,
+    pub mods: Vec<PathBuf>,
+    pub has_server_mods: bool,
+    pub server_mods: Vec<PathBuf>,
+}
+
+pub(crate) fn handle_env_setup(config: PreLaunchSettings) -> PreLaunchMeta {
+    let mods = get_mod_names();
+    let server_mods = get_server_mod_names();
+    let meta = PreLaunchMeta {
+        has_mods: mods.len() > 0,
+        mods,
+        has_server_mods: server_mods.len() > 0,
+        server_mods,
+    };
+
+    debug!("pre-launch metadata: {:?}", meta);
+
     if !config.extra_packages.is_empty() {
         install_packages(config.extra_packages);
     }
+
+    return meta;
 }
 
 fn install_packages(extra_packages: String) {
@@ -24,5 +45,26 @@ fn install_packages(extra_packages: String) {
 
     if !install_status.success() {
         panic!("Failed to install the desired packages. Please check the above output for more information.");
+    }
+}
+
+fn get_mod_names() -> Vec<PathBuf> {
+    get_relative_directories_of("./mods")
+}
+
+fn get_server_mod_names() -> Vec<PathBuf> {
+    get_relative_directories_of("./server_mods")
+}
+
+fn get_relative_directories_of(path: &str) -> Vec<PathBuf> {
+    if let Ok(entries) = fs::read_dir(path) {
+        entries
+            .into_iter()
+            .filter(|r| r.is_ok())
+            .map(|r| r.unwrap().path())
+            .filter(|r| r.is_dir())
+            .collect()
+    } else {
+        return Vec::new();
     }
 }
